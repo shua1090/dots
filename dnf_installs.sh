5732e345ada1ef1f98c86f20a685a56eb915102b
Shynn Lawrence <shynn.lawrence@gmail.com>
2025-09-28 17:45:56 -0700

    some more

--- dnf_installs.sh
+++ dnf_installs.sh
@@ -3,7 +3,7 @@
 sudo dnf copr enable alebastr/sway-extras -y
 
 sudo dnf install \
-    git \
+    git git-lfs gitui zsh\
     hyprland wofi waybar wlogout swaync swww clipman wl-clipboard grim slurp\
     \
     brave-browser kitty \
@@ -16,7 +16,10 @@ sudo dnf install \
     \
     golang rust cargo \
     \
-    install openssl-devel alsa-lib-devel dbus-devel
+    openssl-devel alsa-lib-devel dbus-devel \
+    sh-autosuggestions zsh-syntax-highlighting \
+
 
 flatpak install flathub com.spotify.Client
 cargo install spotify_player --locked
+cargo install starship --locked
--- stow.sh
+++ stow.sh
@@ -3,7 +3,7 @@
 # stow -t ~ gitconfig vimrc
 # sudo stow -t / greetd
 
-stow -t ~ gitconfig vimrc waybar
+stow -t ~ gitconfig vimrc waybar starship
 stow -t ~/.config/hypr hypr
 stow -t ~ wofi wlogout wallpapers swaync
 
