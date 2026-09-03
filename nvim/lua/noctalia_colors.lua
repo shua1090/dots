ae58f8dd36b16fbff0bd6cd8005758ae56978e58
Shynn Lawrence <shynn.lawrence@gmail.com>
2026-09-03 13:42:22 -0700

    colors

--- nvim/lua/noctalia_colors.lua
+++ nvim/lua/noctalia_colors.lua
@@ -1,21 +1,21 @@
 -- Noctalia dynamic colors (Matugen 2.4.1 Syntax)
 local M = {
-    base00 = '#161827',          -- Background
-    base01 = '#2c304f',   -- Lighter background
-    base02 = '#242842', -- Selection/Highlight
-    base03 = '#afb0b6',-- Comments
+    base00 = '#151f28',          -- Background
+    base01 = '#2a3e51',   -- Lighter background
+    base02 = '#233443', -- Selection/Highlight
+    base03 = '#afb4b6',-- Comments
     base04 = '#f2f2f3',        -- Dark foreground
     base05 = '#f2f2f3',        -- Default foreground
     base06 = '#f2f2f3',        -- Light foreground
     base07 = '#f2f2f3',        -- Bright foreground
-    base08 = '#6778e4',           -- Variables/Red
-    base09 = '#875cd6',         -- Integers/Orange
-    base0A = '#be66cc',          -- Classes/Yellow
-    base0B = '#bec5f4',     -- Strings/Green
-    base0C = '#d1bef4',   -- Support/Cyan
-    base0D = '#ecbef4',    -- Functions/Blue
-    base0E = '#6778e4',           -- Keywords/Magenta
-    base0F = '#875cd6',         -- Deprecated/Brown
+    base08 = '#67bce4',           -- Variables/Red
+    base09 = '#5c72d6',         -- Integers/Orange
+    base0A = '#8666cc',          -- Classes/Yellow
+    base0B = '#bee2f4',     -- Strings/Green
+    base0C = '#bec8f4',   -- Support/Cyan
+    base0D = '#cfbef4',    -- Functions/Blue
+    base0E = '#67bce4',           -- Keywords/Magenta
+    base0F = '#5c72d6',         -- Deprecated/Brown
 }
 
 return M
